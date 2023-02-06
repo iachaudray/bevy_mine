c011bee45f17d05003bcd5daaed36ba5ecc52ac7
iachaudray <ibaadchaudray@gmail.com>
2023-02-05 21:29:37 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use bevy::{prelude::*, window::close_on_esc};
+
+fn main() {
+    println!("Hello, world!");
+    App::new().add_plugins(DefaultPlugins)
+    .add_system(close_on_esc)
+    .add_startup_system(setup)
+    .run();
+}
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        sprite: Sprite { color: (Color::rgb(1.0, 1.0, 1.0)), ..default()},
+        transform: Transform { translation: (Vec3::new(0.0, 0.0, 0.0)), rotation: (Quat::from_axis_angle(Vec3::X, 0.0)), scale: (Vec3::splat(5.0)) },
+        ..default()
+    }).insert(Body {});
+
+}
+struct Vec2xf64 {
+    pub x: f64,
+    pub y: f64
+}
+impl Vec2xf64 {
+    fn new(first: f64, second: f64) -> Self {
+        Vec2xf64 { x: first, y: second }s
+    }
+}
+
+impl Default for Body {
+    fn default() -> Self {
+        Body { pos: (), vel: (), mass: () }
+    }
+}
+struct Body {
+    pub pos: Vec2xf64,
+    pub vel: Vec2xf64,
+    pub mass: f64
+}
